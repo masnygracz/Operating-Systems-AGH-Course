b589c2b3fc0fb2609dfc61eb24f8dffd436afb0f
Jakub Woszczek <icefreestyle2000@gmail.com>
2025-03-05 19:19:27 +0100

    Lab1 prepared to reviev

--- lab1/cmake-build-debug/cmake_install.cmake
+++ lab1/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/icefr/Collage_prj/SysOpy/Lab1
+# Install script for directory: C:/Kubiszon/Studia/Sem4/SysOpy/sysopRepo/lab1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/icefr/Collage_prj/SysOpy/Lab1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Kubiszon/Studia/Sem4/SysOpy/sysopRepo/lab1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
