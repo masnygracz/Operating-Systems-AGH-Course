de3eb4cabd5434028f669d960c14b495470d41a8
Jakub Woszczek <icefreestyle2000@gmail.com>
2025-03-18 11:24:04 +0100

    Cleanup & dodałem dyrektywe procesora od dynamicznej

--- lab2/src/collatz.h
+++ lab2/src/collatz.h
@@ -1,8 +1,7 @@
-#ifndef LAB2_LIBRARY_H
-#define LAB2_LIBRARY_H
+#ifndef COLLATZ_H
+#define COLLATZ_H
 
 int collatz_conjecture(int input);
 int test_collatz_convergence(int input, int max_iter, int *steps);
 
-
-#endif //LAB2_LIBRARY_H
+#endif /* COLLATZ_H */
