32984e3ff6460b60911e81015b5cd62dd5ebe77a
Jakub Woszczek <icefreestyle2000@gmail.com>
2025-03-24 13:29:01 +0100

    Setup

---
+++ lab2/cmake-build-debug/CMakeFiles/lab2.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab2.dir/src/collatz.c.obj"
+  "CMakeFiles/lab2.dir/src/collatz.c.obj.d"
+  "CMakeFiles/lab2.dir/src/main.c.obj"
+  "CMakeFiles/lab2.dir/src/main.c.obj.d"
+  "liblab2.dll"
+  "liblab2.dll.a"
+  "liblab2.dll.manifest"
+  "liblab2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
